b40462da30dd66e0558494b62e96dd00827d2f90
SaravanaPriya <saravanapriya.msp31@gmail.com>
2025-02-18 00:21:34 +0530

    adding next set of programs

--- arrayprob/WordPrint.java
+++ arrayprob/WordPrint.java
@@ -1,17 +1,44 @@
 package arrayprob;
 
+import stringset.StringCompare;
+
 public class WordPrint {
 
 	public static void main(String[] args) 
 	{
-		System.out.println(numToWord("54"));
+		numToWord(0);
+		numToWord(4);
+		numToWord(44);
+		numToWord(14);
+		numToWord(50);
+		numToWord(99);
+		numToWord(96);
+		numToWord(10);
+		numToWord(15);
+		numToWord(20);
 
 	}
-	public static String numToWord(String str)
+	public static void numToWord(int num)
 	{
-		String ones[]= {"zero","one","Two","Three","four","five","six","seven","eight","nine","Ten"};
-		String tens[]= {"Eleven","Twelve","Thirteen","fourteen","fifteen","Sixteen","Seventeen","Eighteen","Nineteen","twenty"};
-		String tens2[]= {"Thirty","Forty","fifty","Sixty","seventy","Eighty","ninety"};
+		String ones[]= {"zero","one","Two","Three","four","five","six","seven","eight","nine","Ten","Eleven","Twelve","Thirteen","fourteen","fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+		String tens2[]= {"twenty","Thirty","Forty","fifty","Sixty","seventy","Eighty","ninety","Hundred"};
+		
+		if(num<=19 )
+		{
+			System.out.println(ones[num]);
+				
+		}
+		else if(num%10 !=0)
+		{
+			System.out.println(tens2[num/10 -2] +" "+ones[num%10]);
+		}
+		else
+		{
+			System.out.println(tens2[num/10 -2]);
+		}
+
+					
+		
 		
 	}
 
